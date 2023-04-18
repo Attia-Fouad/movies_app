d524df957351d590f3878b17d70ee1aec0e8297d
Attia <attiafouad04@gmail.com>
2023-04-18 17:56:15 +0200

    finish the app

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: Colors.grey.shade900,
       ),
-      home:  MainMoviesScreen(),
+      home:  const MainMoviesScreen(),
     );
   }
 }
